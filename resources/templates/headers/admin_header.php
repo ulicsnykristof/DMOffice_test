80766178f34eea399f2473ec987e89d12ab51eec
ulicsnykristof <ulicsny.kristof@gmail.com>
2019-10-23 11:00:34 +0200

    header fix

--- resources/templates/headers/admin_header.php
+++ resources/templates/headers/admin_header.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/jpg" href="../pictures/icon.jpg">
-    <link rel="stylesheet" type="text/css" href="../style/my-style.css">
-    <link rel="stylesheet" type="text/css" href="../style/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../style/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../public_html/style/my-style.css">
+    <link rel="stylesheet" type="text/css" href="../../../public_html/style/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../../../public_html/style/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
