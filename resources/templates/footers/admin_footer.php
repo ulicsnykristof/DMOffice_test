871f4abb090e54db27b18c1e7eca561f295f39ef
ulicsnykristof <ulicsny.kristof@gmail.com>
2019-10-23 10:42:57 +0200

    header, footer changes

---
+++ resources/templates/footers/admin_footer.php
@@ -0,0 +1,19 @@
+<!-- Footer -->
+    <footer class="page-footer">
+      <div class="container center">
+        <div class="text-center">
+          <a href="https://www.facebook.com/GojiraMusic"><i class="fa fa-facebook-official"></i></a>
+          <a href="#"><i class="fa fa-instagram"></i></a>
+          <a href="#"><i class="fa fa-snapchat"></i></a>
+          <a href="#"><i class="fa fa-pinterest"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+        </div>
+        <div class="text-center">
+          <p>We do not respect your privacy. - Powered by <a href="https://www.mushroom.com">Mushroom.com</a></p>
+        </div>
+      </div>
+    </footer>
+
+  </body>
+</html>
--- resources/templates/headers/admin_header.php
+++ resources/templates/headers/admin_header.php
@@ -13,3 +13,39 @@
 
     <title>DMOffice</title>
   </head>
+
+  <body>
+
+    <!--
+    <div>Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    -->
+    <!-- Admin Navbar -->
+    <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
+      <a class="navbar-brand" href="#">DMOFFICE - Admin</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Terms and Conditions</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Help</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+              <?php $asd = $_SESSION['user_uid'];
+                echo $asd;
+              ?>
+            </a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Account</a>
+              <a class="dropdown-item" href="#">Options</a>
+              <a class="dropdown-item" href="../login_page.php">Logout</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
